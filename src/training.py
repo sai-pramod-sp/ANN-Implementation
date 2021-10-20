9f5d8dd5d97bdecd57611b8c0ca8840a67511b45
Pramod <saipramod1619@gmail.com>
2021-10-20 21:45:29 +0530

    save model function added

--- src/training.py
+++ src/training.py
@@ -1,24 +1,34 @@
 import os
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
-from src.utils.model import create_model
+from src.utils.model import create_model, save_model
 import argparse
 
 def training(config_path):
-    config = read_config(config_path)
+    config = config(config_path)
     validation_datasize = config["params"]["validation_datasize"]
     (x_train,y_train),(x_valid,y_valid),(x_test,y_test) = get_data(validation_datasize)
 
-    LOSS_FUNCTION = read_config["params"]["loss_function"]
-    OPTIMIZER = read_config["params"]["optimizer"]
-    METRICS = read_config["params"]["metrics"]
-    no_classes = read_config["params"]["no_classes"]
+    LOSS_FUNCTION = config["params"]["loss_function"]
+    OPTIMIZER = config["params"]["optimizer"]
+    METRICS = config["params"]["metrics"]
+    no_classes = config["params"]["no_classes"]
     
     model = create_model(LOSS_FUNCTION,OPTIMIZER,METRICS,no_classes)
-    EPOCHS = read_config["params"]["epochs"]
+    EPOCHS = config["params"]["epochs"]
     VALIDATION = (x_valid, y_valid)
 
     history = model.fit(x_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
+    
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    model_dir = config["artifacts"]["model_dir"]
+
+    model_dir_path = os.path.join(artifacts_dir, model_dir)
+    os.makedirs(model_dir_path, exist_ok = True)
+
+    model_name = config["artifacts"]["model_name"]
+
+    save_model(model, model_name, model_dir_path)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
@@ -28,3 +38,5 @@ if __name__ == '__main__':
     parsed_args = args.parse_args()
 
     training(config_path=parsed_args.config)
+
+
